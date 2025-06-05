ba0241138499246971357f958093eeaa78586b25
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-04 20:48:04 -0400

    TareaDb.kt

---
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/database/TareaDb.kt
@@ -0,0 +1,17 @@
+package edu.ucne.joserivera_ap2_p1.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.joserivera_ap2_p1.data.local.dao.Tareadao
+import edu.ucne.joserivera_ap2_p1.data.local.entities.TareaEntity
+
+@Database(
+    entities = [
+        TareaEntity::class
+    ],
+    version = 5,
+    exportSchema = false
+)
+abstract class TareaDb :RoomDatabase(){
+    abstract fun Tareadao(): Tareadao
+}
