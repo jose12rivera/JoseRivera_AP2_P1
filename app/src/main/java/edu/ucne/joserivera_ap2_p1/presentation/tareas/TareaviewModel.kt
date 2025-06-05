5b28bc805c9aad7eea2b362c60136525994e08cb
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-04 20:49:16 -0400

    TareaviewModel.kt

---
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaviewModel.kt
@@ -0,0 +1,59 @@
+package edu.ucne.joserivera_ap2_p1.presentation.tareas
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.ucne.joserivera_ap2_p1.data.local.entities.TareaEntity
+import edu.ucne.joserivera_ap2_p1.data.respository.TareaRespository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class TareaviewModel (
+    private val tareaRespository: TareaRespository
+): ViewModel(){
+    private val _tarealist= MutableStateFlow<List<TareaEntity>>(emptyList())
+    val tarealist:StateFlow<List<TareaEntity>> get() = _tarealist
+
+    init {
+        LoadTareas()
+    }
+
+    fun LoadTareas(){
+        viewModelScope.launch {
+            tareaRespository.getAll().collect { Lista->
+                Log.d("TareaviewModel","Lista recibida:${Lista.size}")
+                _tarealist.value=Lista
+            }
+        }
+    }
+    fun update(tarea: TareaEntity){
+     save(tarea)
+    }
+
+    fun Agregartarea(descripcion: String,tiempo: Double){
+        val tarea= TareaEntity(
+            tareaid = null,
+            descripcion=descripcion,
+            tiempo=tiempo
+        )
+        save(tarea)
+    }
+    fun save(tarea: TareaEntity){
+        viewModelScope.launch {
+            tareaRespository.save(tarea)
+            LoadTareas()
+        }
+    }
+    fun tareadelete(tarea: TareaEntity){
+        viewModelScope.launch {
+            tareaRespository.delete(tarea)
+            LoadTareas()
+        }
+    }
+
+    fun gettareaIDBY(id: Int): TareaEntity?{
+        return  _tarealist.value.find { it.tareaid==id }
+    }
+
+}
