48dc430d70d0eaa5487fd8531dadab4f80c1e8df
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-07 11:17:09 -0400

    TareaUiState.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaUiState.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaUiState.kt
@@ -1,8 +1,11 @@
 package edu.ucne.joserivera_ap2_p1.presentation.tareas
 
+import edu.ucne.joserivera_ap2_p1.data.local.entities.TareaEntity
+
 data class TareaUiState(
     val tareaId: Int? = null,
     val descripcion: String = "",
-    val tiempo: String = "",
-    val errorMessage: String? = null
+    val tiempo: Int = 0,
+    val errorMessage: String? = null,
+    val tareas: List<TareaEntity> = emptyList()
 )
