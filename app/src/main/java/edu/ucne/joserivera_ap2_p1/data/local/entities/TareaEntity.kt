4790a7442235b3215891314c0cb33dca238ef7da
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-07 11:15:04 -0400

    TareaEntity.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/entities/TareaEntity.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/entities/TareaEntity.kt
@@ -1,13 +1,13 @@
+// data/local/entities/TareaEntity.kt
 package edu.ucne.joserivera_ap2_p1.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Tareas")
-data class TareaEntity(
-    @PrimaryKey
-    val tareaid: Int=0,
-    val descripcion: String="",
-    val tiempo:  Int=0
-
+data class TareaEntity (
+    @PrimaryKey(autoGenerate = true)
+    val tareaId: Int = 0,
+    val descripcion: String = "",
+    val tiempo: Int = 0
 )
