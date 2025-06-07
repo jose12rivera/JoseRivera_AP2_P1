b4eba4c4539bd018c1ce7eabb9ef0495f0162c34
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-06 22:42:47 -0400

    TareaListScreen.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaListScreen.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaListScreen.kt
@@ -1,24 +1,13 @@
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+package edu.ucne.joserivera_ap2_p1.presentation.tareas
+
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -58,8 +47,7 @@ fun TareaListScreen(
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF6650a4),
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
 
@@ -71,7 +59,7 @@ fun TareaListScreen(
             ) {
                 items(tarealist) { tarea ->
                     TareaRow(
-                        tarea,
+                        tarea = tarea,
                         onDelete = { onDelete(it.tareaid) },
                         onEditar = onEditar
                     )
@@ -80,8 +68,6 @@ fun TareaListScreen(
         }
     }
 }
-
-
 
 @Composable
 fun TareaRow(
