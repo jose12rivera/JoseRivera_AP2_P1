45c2e2e1c60e274f97dfec34ecde2706535d1465
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-07 10:56:13 -0400

    MainActivity.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.joserivera_ap2_p1.presentation.navigation.AppNavigation
-import edu.ucne.joserivera_ap2_p1.presentation.tareas.TareaviewModel
 import edu.ucne.joserivera_ap2_p1.ui.theme.JoseRivera_AP2_P1Theme
 
 @AndroidEntryPoint
@@ -19,18 +17,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JoseRivera_AP2_P1Theme() {
+            JoseRivera_AP2_P1Theme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    val viewModel: TareaviewModel = hiltViewModel()
-
-                    AppNavigation(
-                        navController = navController,
-                        viewModel = viewModel
-                    )
+                    AppNavigation(navController = navController)
                 }
             }
         }
