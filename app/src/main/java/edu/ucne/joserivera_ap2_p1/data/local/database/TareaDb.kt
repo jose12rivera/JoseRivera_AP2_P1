d41481bab393c27164427fe7caaa7b12f65a3136
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-06 22:41:51 -0400

    TareaDb.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/database/TareaDb.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/database/TareaDb.kt
@@ -7,7 +7,7 @@ import edu.ucne.joserivera_ap2_p1.data.local.entities.TareaEntity
 
 @Database(
     entities = [TareaEntity::class],
-    version = 8,
+    version = 9,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
