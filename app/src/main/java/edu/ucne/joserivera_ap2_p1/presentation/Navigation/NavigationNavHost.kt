579d8e7016bde8d3893a62f7f0a88ea74d7bfb8f
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-07 10:56:46 -0400

    NavigationNavHost.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/Navigation/NavigationNavHost.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/Navigation/NavigationNavHost.kt
@@ -1,86 +1,40 @@
-// AppNavigation.kt
 package edu.ucne.joserivera_ap2_p1.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import edu.ucne.joserivera_ap2_p1.presentation.HomeScreen
 import edu.ucne.joserivera_ap2_p1.presentation.tareas.*
 
 @Composable
-fun AppNavigation(
-    navController: NavHostController,
-    viewModel: TareaviewModel
-) {
-    val tarealist by viewModel.tarealist.collectAsState()
-    val uiState by viewModel.uiState.collectAsState()
-
+fun AppNavigation(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = "tareas"
     ) {
-        composable("home") { HomeScreen(navController = navController) }
-
-        composable("tarea_list") {
+        composable("tareas") {
             TareaListScreen(
-                tarealist = tarealist,
-                onCreate = { navController.navigate("tarea_nueva") },
-                onDelete = { tareaId -> navController.navigate("eliminar_tarea/$tareaId") },
-                onEditar = { tareaId -> navController.navigate("editar_tarea/$tareaId") }
+                goToTarea = { tareaId -> navController.navigate("tarea/$tareaId") },
+                createTarea = { navController.navigate("tarea/0") }
             )
         }
 
-        composable("tarea_nueva") {
-            // Resetear el estado para nueva tarea
-            LaunchedEffect(Unit) {
-                viewModel.loadTarea(0)
-            }
-            TareaEditarScreen(
-                tareaId = 0,
-                viewModel = viewModel,
-                onGuardar = { navController.popBackStack() },
-                onCancelar = { navController.popBackStack() }
-            )
-        }
-
-        composable("editar_tarea/{tareaId}") { backStackEntry ->
-            val tareaId = backStackEntry.arguments?.getString("tareaId")?.toIntOrNull() ?: 0
-
-            // Cargar la tarea cuando entra a editar
-            LaunchedEffect(tareaId) {
-                if (tareaId > 0) {
-                    viewModel.loadTarea(tareaId)
-                }
-            }
-
-            TareaEditarScreen(
+        composable(
+            "tarea/{tareaId}",
+            arguments = listOf(navArgument("tareaId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val tareaId = backStackEntry.arguments?.getInt("tareaId") ?: 0
+            TareaScreen(
                 tareaId = tareaId,
-                viewModel = viewModel,
-                onGuardar = { navController.popBackStack() },
-                onCancelar = { navController.popBackStack() }
+                goBack = { navController.popBackStack() }
             )
-        }
-
-        composable("eliminar_tarea/{tareaId}") { backStackEntry ->
-            val tareaId = backStackEntry.arguments?.getString("tareaId")?.toIntOrNull() ?: 0
-            val tarea = viewModel.getTareaById(tareaId)
-
-            if (tarea != null) {
-                DeleteTareaScreen(
-                    tarea = tarea,
-                    onDelete = {
-                        viewModel.deleteTarea(it)
-                        navController.popBackStack()
-                    },
-                    onCancel = { navController.popBackStack() }
-                )
-            } else {
-                navController.popBackStack()
-            }
         }
     }
 }
