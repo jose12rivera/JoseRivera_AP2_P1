0651ba425015b34bd67e495f29f9d3735c1edc89
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-07 11:15:36 -0400

    TareaEvent.kt

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaEvent.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/tareas/TareaEvent.kt
@@ -1,5 +1,11 @@
-sealed class TareaEvent {
-    data class DescripcionChange(val descripcion: String) : TareaEvent()
-    data class TiempoChange(val tiempo: String) : TareaEvent()
-    object Save : TareaEvent()
+package edu.ucne.joserivera_ap2_p1.presentation.tareas
+sealed interface TareaEvent {
+    data class DescripcionChange(val descripcion: String) : TareaEvent
+    data class TiempoChange(val tiempo: Int) : TareaEvent
+    data class LoadTarea(val tareaId: Int) : TareaEvent
+    object Save : TareaEvent
+    object Delete : TareaEvent
+    object New : TareaEvent
+    data class DeleteById(val tareaId: Int) : TareaEvent
+
 }
