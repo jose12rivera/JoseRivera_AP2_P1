bed6ef8d478ffa4873bfd0609033a712eaadd555
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-09-01 12:49:44 -0400

    le cambie el insert por upsert y import

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/dao/Tareadao.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/data/local/dao/Tareadao.kt
@@ -2,19 +2,19 @@ package edu.ucne.joserivera_ap2_p1.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import edu.ucne.joserivera_ap2_p1.data.local.entities.TareaEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TareaDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert()
     suspend fun save(tarea: TareaEntity)
 
-    @Query("SELECT * FROM Tareas WHERE tareaid = :id LIMIT 1")
+    @Query("""
+        SELECT * FROM Tareas WHERE tareaid = :id LIMIT 1""")
     suspend fun find(id: Int): TareaEntity?
 
     @Delete
