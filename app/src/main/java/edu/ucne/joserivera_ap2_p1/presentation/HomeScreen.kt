877afc0d7742d4f4f2c1304ff1b1cb813e4bbbc0
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-09-01 12:44:10 -0400

    Le lo import

--- app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/HomeScreen.kt
+++ app/src/main/java/edu/ucne/joserivera_ap2_p1/presentation/HomeScreen.kt
@@ -2,10 +2,23 @@ package edu.ucne.joserivera_ap2_p1.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material3.*
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,7 +50,7 @@ fun HomeScreen(
         ) {
             MenuCard(
                 titulo = "Tareas",
-                icono = Icons.Default.List,
+                icono = Icons.Default.Home,
                 onClick = { navController.navigate("tarea_list") }
             )
         }
